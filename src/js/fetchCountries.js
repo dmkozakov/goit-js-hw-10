3f538503644b940abac29c3b8bbd3c889404900a
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-04-10 13:19:42 +0300

    bugfix

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -7,7 +7,6 @@ export function fetchCountries(name) {
     `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (!response.ok) {
-      Notify.failure("Oops, there is no country with that name");
       throw new Error(response.status);
     }
     return response.json();
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
+import { refs } from "./refs";
+import {
+  makeCountryInfoItem,
+  makeCountryListItem,
+  resetMarkup,
+} from "./markup";
 import { fetchCountries } from "./fetchCountries.js";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 import "../css/styles.css";
 
 const DEBOUNCE_DELAY = 300;
 const debounce = require("lodash.debounce");
-
-const refs = {
-  searchInput: document.querySelector("#search-box"),
-  countryList: document.querySelector(".country-list"),
-  countryInfo: document.querySelector(".country-info"),
-};
 
 refs.searchInput.addEventListener(
   "input",
@@ -22,14 +22,13 @@ refs.searchInput.addEventListener(
 function onSearch(e) {
   const searchQuery = e.target.value.toLowerCase().trim();
 
-  refs.countryList.innerHTML = "";
-  refs.countryInfo.innerHTML = "";
+  resetMarkup();
 
   if (!searchQuery) {
     return;
   }
 
-  fetchCountries(searchQuery).then(renderCountryInfo).catch(console.log);
+  fetchCountries(searchQuery).then(renderCountryInfo).catch(onReject);
 }
 
 function renderCountryInfo(data) {
@@ -47,23 +46,6 @@ function renderCountryInfo(data) {
   refs.countryInfo.insertAdjacentHTML("beforeend", countryInfoMarkup);
 }
 
-function makeCountryListItem(country) {
-  return `
-  <li class="country-list__item">
-    <img  src="${country.flags.svg}" alt="${country.name.common} flag" width="50">
-    <p>${country.name.common}</p>
-  </li>`;
-}
-
-function makeCountryInfoItem(country) {
-  return `
-  <div class="country-name">
-    <img src="${country[0].flags.svg}" 
-    alt="${country[0].name.common} flag" width="50" />
-    <h1>${country[0].name.common}</h1>
-  </div>
-  <p><b>Capital:</b> ${country[0].capital}</p>
-  <p><b>Population:</b> ${country[0].population}</p>
-  <p><b>Languages:</b> ${Object.values(country[0].languages)}</p>
-`;
+function onReject() {
+  return Notify.failure("Oops, there is no country with that name");
 }
